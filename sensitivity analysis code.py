aa277a0b639f9a0ab074a76953839ffdb84e2845
LL <lilin96@nwafu.edu.cn>
2025-02-23 15:23:11 +0800

    sensitivity analysis code.py

--- sensitivity analysis code.py
+++ sensitivity analysis code.py
@@ -1,37 +1,63 @@
-import matplotlib.pyplot as plt
-from doubleml import DoubleMLData, DoubleMLPLR
-from sklearn.ensemble import RandomForestRegressor
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.neural_network import MLPRegressor, MLPClassifier
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import LassoCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+import doubleml as dml
 
-# CSV data
-data_path = "data.csv"
-data = pd.read_csv(data_path)
+file_path = r"C:\Users\lenovo\Desktop\12699\data.csv" 
+data = pd.read_csv(file_path)
 
-# y, d, X
-y_col = 'resilience'
-d_col = 'digital'
-X_cols = ['age', 'age_sq', 'edu', 'farmtime', 'health', 'party', 'labor', 
-          'plant_demons', 'farmland_area', 'consolidation', 'certification', 
-          'income', 'agtraing', 'emtraing', 'insurance', 'loan', 'water', 
-          'coll_economic', 'distance', 'capabilities', 'drought', 'frost', 
-          'credit', 'policy', 'technology_support']
+data.head() 
+y_col = 'resilience'  
+d_col = 'digital' 
+X_cols = ['age', 'age_sq', 'edu', 'farmtime', 'health', 'party', 'labor',
+          'plant_demons', 'farmland_area', 'consolidation', 'certification',
+          'income', 'agtraing', 'emtraing', 'insurance', 'loan', 'water',
+          'coll_economic', 'distance', 'capabilities', 'drought', 'frost',
+          'credit', 'policy', 'technology_support'] 
 
-# DoubleML
-obj_dml_data = DoubleMLData(data, y_col, d_col, X_cols)
+data_dml = dml.DoubleMLData(data, y_col=y_col, d_cols=d_col, x_cols=X_cols)
 
-# learner
-ml_l = RandomForestRegressor()
-ml_m = RandomForestRegressor()
+data_dml
 
-#DoubleMLPLR
-dml_plr = DoubleMLPLR(obj_dml_data, ml_l, ml_m)
+np.random.seed(42)
 
-# fit
-dml_plr.fit()
+#(3)
+learner_l = LinearRegression()
+learner_m = LinearRegression()
+#(4)
+learner_l = RandomForestRegressor(n_estimators=100,  max_features=1, min_samples_leaf=1)
+learner_m = RandomForestClassifier(n_estimators=100,  max_features=1, min_samples_leaf=1)
+#(5)
+learner_l = MLPRegressor(hidden_layer_sizes=(50, 50), max_iter=200)
+learner_m = MLPClassifier(hidden_layer_sizes=(50, 50), max_iter=200)
+#(6)
+learner_l = Ridge(alpha=1.0) 
+learner_m = Ridge(alpha=1.0)
+#(7)
+Cs = 0.0001*np.logspace(0, 4, 10)
+learner_l = make_pipeline(StandardScaler(), LassoCV(cv=5, max_iter=1000))
+learner_m = make_pipeline(StandardScaler(), LassoCV(cv=5, max_iter=1000))
 
-# sensitivity analysis
-sensitivity_analysis = dml_plr.sensitivity_analysis(cf_y=0.03, cf_d=0.03, rho=1.0, level=0.95)
+
+dml_obj = dml.DoubleMLPLR(data_dml, ml_l=learner_l, ml_m=learner_m, n_folds=5, n_rep=1)
+dml_obj.fit()  
+dml_obj.fit(store_predictions=True)
+
+print(" DoubleMLPLR :")
+print(dml_obj)
 
 
+print("confint:")
+print(dml_obj.confint())
 
+print("sensitivity analysis:")
+dml_obj.sensitivity_analysis(cf_y=0.04, cf_d=0.03)  
+print(dml_obj.sensitivity_summary)  
+
+dml_obj.sensitivity_plot()
